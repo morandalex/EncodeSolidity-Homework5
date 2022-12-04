f5db05286de3f161629f1cb1e570c8f704ca93ae
amorandi <a.morandi.info@gmail.com>
2022-12-04 17:17:18 +0100

    simplified next app config with hardhat

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,57 +1,27 @@
-
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-
 import {
   chain,
-  configureChains,
   createClient,
   WagmiConfig,
 } from 'wagmi';
-
-import { infuraProvider } from 'wagmi/providers/infura';
-import { publicProvider } from 'wagmi/providers/public';
-//import {jsonRpcProvider} from 'wagmi/providers/jsonRpc'
-//import { alchemyProvider } from 'wagmi/providers/alchemy';
-
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-
-import { ConnectKitProvider} from 'connectkit'
-
-
-const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.goerli, chain.polygonMumbai],
-  [
-    //   alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-    infuraProvider({ apiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY ?? '' }),
-    publicProvider(),
-  ]
+import { ConnectKitProvider, getDefaultClient } from 'connectkit'
+const infuraId = process.env.ALCHEMY_ID;
+const client = createClient(
+  getDefaultClient({
+    appName: "EncodeSolidity App",
+    infuraId,
+    chains: [chain.goerli, chain.hardhat],
+  }),
 );
-
-const wagmiClient = createClient({
-  autoConnect: false,
-  provider,
-  connectors: [
-    new MetaMaskConnector(),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-  ],
-})
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <WagmiConfig client={wagmiClient}>
-      <ConnectKitProvider theme="auto" mode="dark">
+      <WagmiConfig client={client}>
+        <ConnectKitProvider theme="auto" mode="dark" /*options={{ initialChainId: chain.hardhat.id }}*/>
           <Component {...pageProps} />
-          </ConnectKitProvider>
+        </ConnectKitProvider>
       </WagmiConfig>
     </ChakraProvider>
   )
 }
-
---
+++ hardhat/scripts/deployLocal.ts
@@ -0,0 +1,46 @@
+//before starting this script you have to start hardhat node with `yarn hardhat node`
+
+const fs = require('fs');
+import { ethers } from "hardhat";
+import { Lottery__factory, Lottery  } from "../typechain-types";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const PURCHASE_RATIO = 1;
+const BET_PRICE = 1;
+const BET_FEE = 0.1;
+
+async function main() {
+    // To connect to a custom URL:
+    let url = "http://127.0.0.1:8545/";
+    let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+    const signer = new ethers.Wallet(process.env.HARDHAT_ACCOUNT_0 ?? "").connect(
+        customHttpProvider
+    );
+
+    const lotteryContractFactory = new Lottery__factory(signer);
+    const lotteryContract = await lotteryContractFactory.deploy(
+    PURCHASE_RATIO,
+    ethers.utils.parseEther(BET_PRICE.toFixed(18)),
+    ethers.utils.parseEther(BET_FEE.toFixed(18))
+    );
+    await lotteryContract.deployed();
+
+    const paymentTokenAddress = await lotteryContract.paymentToken();
+    const paymentTokenFactory = await ethers.getContractFactory("LotteryToken");
+    const paymentToken = paymentTokenFactory.attach(paymentTokenAddress);
+
+    console.log(
+    `The lottery contract has been deployed at address ${lotteryContract.address}`
+    );
+    console.log(
+    `The payment token contract has been deployed at address ${paymentToken.address}`
+    );
+
+    fs.writeFileSync('local_deploy.txt', `NEXT_PUBLIC_LOTTERY_CONTRACT="${lotteryContract.address}"`  + '\n' + `NEXT_PUBLIC_PAYMENT_TOKEN_CONTRACT="${paymentToken.address}"`  );
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
