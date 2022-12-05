2f69fdc4ab9f37bdad7715795684f10cc58659df
amorandi <a.morandi.info@gmail.com>
2022-12-05 19:20:52 +0100

    update frontend funs

--- frontend/components/ListFunctions.tsx
+++ frontend/components/ListFunctions.tsx
@@ -7,10 +7,10 @@ import { Box, Button, Text, Link } from "@chakra-ui/react";
 import { useAccount, useNetwork, useProvider, useSigner } from "wagmi";
 
 //import custom utils
-import utils from '../src/utils'
+import appSdk from '../src/utils'
 
 //import ethers
-import ethers, { Signer } from 'ethers'
+import ethers, { BigNumber, utils } from 'ethers'
 
 //here we have types from hardhat compiles
 import { Lottery__factory, LotteryToken__factory } from "../typechain-types";
@@ -29,16 +29,23 @@ export default function ListFunctions() {
 
 
 
-    const { address, isConnecting, isDisconnected } = useAccount()
+    const { address: userAddress, isConnecting, isDisconnected } = useAccount()
     const { chain, chains } = useNetwork()
     const provider = useProvider();
     const { data: signer, isSuccess: signerLoaded } = useSigner();
     const [str, setStr] = useState(' hello world');
     const [lotteryContractAddress, setLotteryContractAddress] = useState('')
     const [lotteryTokenContractAddress, setLotteryTokenContractAddress] = useState('')
-    const [txList, setTxList] = useState([{ chainId: 0, tx: '' }])
+    const [txList, setTxList] = useState([{ chainId: 0, tx: '', funName: '' }])
+    const [balance, setbalance] = useState<string>('')
+    const [contractStatus, setContractStatus] = useState(false)
+    const [contractCloseLottery, setContractCloseLottery] = useState(false)
 
+    useEffect(()=>{
 
+        console.log(txList)
+
+    },[txList])
     //useEffect is a react hook that normally is used to init tha app. Here we use useEffect to reinitialize the contract every time the chain changes
     useEffect(() => {
         if (chain) {
@@ -53,11 +60,30 @@ export default function ListFunctions() {
                     setLotteryContractAddress('')
                     setLotteryTokenContractAddress('')
                 }
+
+
         }
 
     }, [chain])  //if we put a variable inside this array useEffect will recompute itself
 
+    useEffect(() => {
+        checkStatus()
 
+    }, [signer])
+    function addToTxList(txhash: string, funName: string) {
+
+        if (txhash && txhash != '') {
+            setTxList(
+                oldTxList => [
+                    ...oldTxList,
+                    {
+                        chainId: chain?.id ?? 0,
+                        tx: txhash,
+                        funName: funName
+                    }
+                ]);
+        }
+    }
     async function openBets() {
         if (signerLoaded && signer && lotteryContractAddress != '' && lotteryTokenContractAddress != '') {
 
@@ -72,14 +98,7 @@ export default function ListFunctions() {
                 const receipt = await openBetsTx.wait();
                 console.log(`Bets opened (${receipt.transactionHash})`);
 
-                setTxList(
-                    oldTxList => [
-                        ...oldTxList,
-                        {
-                            chainId: chain?.id ?? 0,
-                            tx: receipt.transactionHash
-                        }
-                    ]);
+                addToTxList(receipt.transactionHash, 'openBets')
 
 
 
@@ -91,6 +110,184 @@ export default function ListFunctions() {
             alert('error')
         }
     }
+    async function myBalance() {
+        if (
+            signerLoaded &&
+            signer &&
+            lotteryContractAddress != "" &&
+            lotteryTokenContractAddress != ""
+        ) {
+            const tokenContractFactory = new LotteryToken__factory(signer);
+
+            const tokenContract = tokenContractFactory.attach(
+                lotteryTokenContractAddress ?? ""
+            );
+            const currentBlock = await provider.getBlock("latest");
+
+            const addr = await signer.getAddress();
+            const b = await tokenContract.balanceOf(addr);
+            setbalance(b.toString())
+
+
+
+            try {
+                console.log(`Balance of ${b} tokens`);
+            } catch (e: any) {
+                alert(e?.reason);
+            }
+        } else {
+            alert("error");
+        }
+    }
+    async function buyTokens() {
+        if (
+            signerLoaded &&
+            signer &&
+            lotteryContractAddress != "" &&
+            lotteryTokenContractAddress != ""
+        ) {
+            const lotteryContractFactory = new Lottery__factory(signer);
+            const tokenContractFactory = new LotteryToken__factory(signer);
+
+            const lotteryContract = lotteryContractFactory.attach(
+                lotteryContractAddress ?? ""
+            );
+            const tokenContract = tokenContractFactory.attach(
+                lotteryTokenContractAddress ?? ""
+            );
+
+            try {
+                const purchaseTx = await lotteryContract.purchaseTokens({
+                    value: utils.parseEther('0.01'),
+                });
+                let receipt = await purchaseTx.wait()
+                addToTxList(receipt.transactionHash, 'purchaseTokens')
+
+
+            } catch (e: any) {
+                alert(e);
+            }
+        } else {
+            alert("error");
+        }
+    }
+
+    async function bet() {
+        if (
+            signerLoaded &&
+            signer &&
+            lotteryContractAddress != "" &&
+            lotteryTokenContractAddress != ""
+        ) {
+            const lotteryContractFactory = new Lottery__factory(signer);
+            const tokenContractFactory = new LotteryToken__factory(signer);
+
+            const lotteryContract = lotteryContractFactory.attach(
+                lotteryContractAddress ?? ""
+            );
+            const tokenContract = tokenContractFactory.attach(
+                lotteryTokenContractAddress ?? ""
+            );
+
+
+
+            try {
+                const amount = 10000000000
+
+                const value = BigNumber.from(amount).mul(utils.parseEther((BET_FEE + BET_PRICE).toString()));
+                const allowTx = await tokenContract.connect(signer).approve(lotteryContract.address, value);
+                let receipt1 = await allowTx.wait();
+
+
+                addToTxList(receipt1.transactionHash, 'approve')
+
+                const tx = await lotteryContract.bet();
+                const receipt2 = await tx.wait();
+                const gasCost = receipt2.gasUsed.mul(receipt2.effectiveGasPrice);
+                console.log(`${receipt2.gasUsed} units of gas were consumed at ${receipt2.effectiveGasPrice} gas price fro ${utils.formatEther(gasCost)}`);
+                addToTxList(receipt2.transactionHash, 'bet')
+            } catch (e: any) {
+                alert(e);
+            }
+        } else {
+            alert("error");
+        }
+    }
+
+    async function checkStatus() {
+        if (
+            signerLoaded &&
+            signer &&
+            lotteryContractAddress != "" &&
+            lotteryTokenContractAddress != ""
+        ) {
+            const lotteryContractFactory = new Lottery__factory(signer);
+            const tokenContractFactory = new LotteryToken__factory(signer);
+
+            const lotteryContract = lotteryContractFactory.attach(
+                lotteryContractAddress ?? ""
+            );
+            const tokenContract = tokenContractFactory.attach(
+                lotteryTokenContractAddress ?? ""
+            );
+
+
+
+            try {
+                const state = await lotteryContract.betsOpen();
+                setContractStatus(state)
+
+
+            } catch (e: any) {
+                alert(e);
+            }
+        } else {
+            console.log("not yet connected to load the checkStatus function");
+        }
+
+
+
+    }
+
+    async function closeLottery() {
+        if (
+            signerLoaded &&
+            signer &&
+            lotteryContractAddress != "" &&
+            lotteryTokenContractAddress != ""
+        ) {
+            const lotteryContractFactory = new Lottery__factory(signer);
+            const tokenContractFactory = new LotteryToken__factory(signer);
+
+            const lotteryContract = lotteryContractFactory.attach(
+                lotteryContractAddress ?? ""
+            );
+            const tokenContract = tokenContractFactory.attach(
+                lotteryTokenContractAddress ?? ""
+            );
+
+
+
+            try {
+                const tx = await lotteryContract.closeLottery();
+                const receipt = await tx.wait()
+                addToTxList(receipt.transactionHash, 'closeLottery')
+
+
+            } catch (e: any) {
+                alert(e);
+            }
+        } else {
+            console.log("error");
+        }
+
+
+
+    }
+
+
+
+
 
     if (!isDisconnected && !isConnecting) {
         return (
@@ -103,37 +300,67 @@ export default function ListFunctions() {
                     p="5"
                 >
                     <Text>Chain  : {chain && chain.name}</Text>
-                    <Text> Lottery Contract : {utils.cutAddress(lotteryContractAddress)}</Text>
-                    <Text>Lottery token Contract : {utils.cutAddress(lotteryTokenContractAddress)}</Text>
-                    <Button onClick={openBets}>openBets()</Button>
-                </Box>
-                <Box
+                    <Text> Lottery Contract : {appSdk.cutAddress(lotteryContractAddress)}</Text>
+                    <Text>Lottery token Contract : {appSdk.cutAddress(lotteryTokenContractAddress)}</Text>
+                    <Text>Bets are open? : {contractStatus ? 'yes' : 'nope'}</Text>
+
+                    {balance && balance != '' && <Text>This is my balance : {balance}</Text>}
+
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        flexDirection="column"
+                        alignItems="center"
+                        p="5">
+                        <Text>Owner functions</Text>
+                        <Button m='2' onClick={openBets}>openBets()</Button>
+                    </Box>
+
+                    <Box
+                        display="flex"
+                        justifyContent="center"
+                        flexDirection="column"
+                        alignItems="center"
+                        p="5"
+                    >
+
+                        <Text>Users functions</Text>
+                        <Button m='2' onClick={buyTokens}>buyTokens()</Button>
+                        <Button m='2' onClick={myBalance}>mybalance()</Button>
+                        <Button m='2' onClick={bet}>bet()</Button>
+                        <Button m='2' onClick={closeLottery}>closeLottery()</Button>
+                    </Box>
+
+                    <Box
                     display="flex"
                     justifyContent="center"
                     flexDirection="column"
                     alignItems="center"
                     p="5"
                 >
+                    <Text>Transactions list</Text>
                     {
                         txList.map((item, i) => {
                             if (chain?.id == 31337 && item.tx != '' && item.chainId == 31337)
                                 return (
-                                    <>
+                                  
 
-                                        <Text>{utils.cutAddress(item.tx)}</Text>
-                                    </>
+                                        <Text key = {i}>{appSdk.cutAddress(item.tx)}</Text>
+                                   
                                 )
-                            else if (chain?.id == 5 && item.tx != '' && item.chainId == 5)
+                            else if (chain?.id == 5 && item.tx != '')
                                 return (
-                                    <>
+                                   
 
-                                        <Link href={"https://goerli.etherscan.io/tx/" + item.tx} isExternal ><Text>{utils.cutAddress(item.tx)}</Text> </Link>
-                                    </>
+                                        <Link key={i} href={"https://goerli.etherscan.io/tx/" + item.tx} isExternal ><Text>  {appSdk.cutAddress(item.funName)} : {appSdk.cutAddress(item.tx)}</Text> </Link>
+                                   
                                 )
 
                         })
                     }
                 </Box>
+                </Box>
+               
             </>
         );
     }
